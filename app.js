72c5dc5adf78146a3e631f9b21f66d7cba177962
martin <martin.lange@csa-germany.de>
2022-03-07 13:55:00 +0100

    add use for root route for testing purposes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { StatusCodes } = require("http-status-codes");
 const app = express();
 const pokemonRouter = require("./pokemon");
 
 // Routes ------
 app.use('/pokemon', pokemonRouter);
+
+app.use((req, res, next) => {
+    res.status(StatusCodes.OK).send('Martin\'s PokeFight API is working ...');
+    next(req, res);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
