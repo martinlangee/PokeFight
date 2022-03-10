34f67ab67607a7325e33d0f11a187dc966f4e9a7
martin <martin.lange@csa-germany.de>
2022-03-10 11:01:10 +0100

    update API description

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ app.use('/pokemon', pokemonRouter);
 app.use((req, res, next) => {
     res.status(StatusCodes.OK).send(
         `<h1>Martin\'s PokeFight API is listening ...</h1>
-         <p>Specify "/pokemon" to get all pokemon\'s data.</p>
-         <p>Specify "/pokemon/:id" to get a specific pokemon\'s data.</p>
-         <p>Specify "/pokemon/:id/:(&lt;name&gt;|&lt;type&gt;|&lt;base&gt;)" to get a specific pokemon\'s info.</p>
-         <p>Specify "/pokemon/image/:id/:to get a pokemon\'s SVG image</p>`);
+         <h3>Here's how to use it:</h3>
+         <p>"/pokemon" to get all pokemon\'s data.</p>
+         <p>"/pokemon/:id" to get a specific pokemon\'s data.</p>
+         <p>"/pokemon/:id/:(&lt;name&gt;|&lt;type&gt;|&lt;base&gt;)" to get a specific pokemon\'s info.</p>
+         <p>"/pokemon/image/:id/:to get a pokemon\'s SVG image</p>`);
     next(req, res);
 });
 
